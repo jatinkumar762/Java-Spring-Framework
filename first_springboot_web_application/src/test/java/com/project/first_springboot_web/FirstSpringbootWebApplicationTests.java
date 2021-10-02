da0f46fe0c2db5e6408e2e15a474c963f39616ee
Jatin <jatin.kumar@1silverbullet.tech>
2021-10-03 00:23:57 +0530

    first spring boot project

---
+++ first_springboot_web_application/src/test/java/com/project/first_springboot_web/FirstSpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.first_springboot_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
